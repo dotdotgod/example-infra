7fb857331a3064cba38eefdd9e1c2c7ac4398eeb
광복 <kblee@artue.io>
2024-04-03 17:56:58 +0900

    base settings

--- example-chat/ssm.tf
+++ example-chat/ssm.tf
@@ -4,3 +4,10 @@ resource "aws_ssm_parameter" "socket_api_gateway_id" {
   type  = "SecureString"
   value = aws_apigatewayv2_api.socket.id
 }
+
+resource "aws_ssm_parameter" "serverless-chat-bucket" {
+  name  = "${var.project_name}-${var.stage}-serverless-chat-bucket"
+  description = "The name of the S3 bucket for the serverless chat service."
+  type  = "SecureString"
+  value = aws_s3_bucket.serverless_chat.bucket
+}
